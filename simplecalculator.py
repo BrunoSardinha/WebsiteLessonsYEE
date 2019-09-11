7ab46489de22043b194efdc19cb9d2b78d936d64
BrunoSardinha <bruno@sapoznik.com.br>
2019-09-11 18:58:04 -0300

    sync EVERYTHING YYYYYYYYYEEEEEEEEEEEEEEEEEETTTTTTTTTTTTTTTTTTTT!

---
+++ simplecalculator.py
@@ -0,0 +1,19 @@
+firstnumber = int(input("first number... "))
+operation = input("what is the operation?... ")
+secondnumber = int(input("second number...  "))
+
+
+if operation == "+":
+    print("result is .. ", firstnumber + secondnumber)
+
+elif operation == "-":
+    print("result is .. ", firstnumber - secondnumber)
+
+elif operation == "*":
+    print("result is .. ", firstnumber * secondnumber)
+
+elif operation == "/":
+    print("result is .. ", firstnumber / secondnumber)
+
+else:
+    print("INVALID. SHUTING. DOWN. :( :( :( :( :(.             LOG OUT AND RESTART.:(        ")
